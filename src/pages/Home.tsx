44b61ce1aef67bae814029fc540f7b82c8c36c84
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-22 22:52:02 +0100

    rotas Home/NewRoom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,22 @@
+import { useHistory } from "react-router-dom";
+
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
 import { Button } from "../components/Button";
 
-import '../styles/auth.scss';
+import "../styles/auth.scss";
 
 export function Home() {
+  const history = useHistory();
+
+  function navigateToNewRoom() {
+      history.push('/rooms/new')
+  }
+
   return (
-    <div id='page-auth'>
+    <div id="page-auth">
       <aside>
         <img
           src={illustrationImg}
@@ -18,21 +26,16 @@ export function Home() {
         <p>Tire as dúvidas da sua audiência em tempo real</p>
       </aside>
       <main>
-        <div className='main-content'>
+        <div className="main-content">
           <img src={logoImg} alt="LetmeAsk" />
-          <button className='create-room'>
+          <button onClick={navigateToNewRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div className='separator'>ou entre em uma sala</div>
+          <div className="separator">ou entre em uma sala</div>
           <form>
-            <input
-            type="text" 
-            placeholder='Digite o código da sala'
-            />
-            <Button type="submit">
-                Entrar na sala
-            </Button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
