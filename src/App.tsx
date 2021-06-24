7c12b994041bf265549b6d7e9f7114e0864e101f
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-24 21:47:15 +0100

    Autorizaçnao

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { type } from "os";
-import { createContext, useState, useEffect } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
-import { auth, firebase } from "./services/firebase";
+import { Room } from "./pages/Room";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
 
@@ -14,8 +12,11 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
+        <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from "react-router-dom";
+import { useState, FormEvent } from "react";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
@@ -8,16 +9,34 @@ import { Button } from "../components/Button";
 
 import "../styles/auth.scss";
 import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 
 export function Home() {
   const history = useHistory();
-  const { user, singInWithGoogle } = useAuth()
+  const { user, singInWithGoogle } = useAuth();
+  const [roomCode, setRoomCode] = useState('');
 
   async function handleCreateRoom() {
-    if(!user){
-      await singInWithGoogle()
+    if (!user) {
+      await singInWithGoogle();
     }
     history.push("/rooms/new");
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === '') {
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exist.");
+      return;
+    }
+    history.push(`/rooms/${roomCode}`);
   }
 
   return (
@@ -38,8 +57,13 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
+            />
             <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
